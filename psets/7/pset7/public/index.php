aeb29e522f28e0dc58e9799f3094ce68b5168b26
captainmoha <admiralmoha@gmail.com>
2016-11-17 02:00:59 +0200

    pset7 add portfolio

--- psets/7/pset7/public/index.php
+++ psets/7/pset7/public/index.php
@@ -3,7 +3,36 @@
     // configuration
     require("../includes/config.php"); 
 
+
+    if (isset($_SESSION["id"]))
+    {
+    	$rows = CS50::query("SELECT symbol, shares from portfolios WHERE user_id = ?", $_SESSION["id"]);
+
+    	$cashArr = CS50::query("SELECT cash from users WHERE id = ?", $_SESSION["id"]);
+    	$cash = $cashArr[0]["cash"];
+
+    	$usernameArr = CS50::query("SELECT username from users WHERE id = ?", $_SESSION["id"]);
+    	$username = $usernameArr[0]["username"];
+    }
+    
+    $positions = [];
+
+    foreach($rows as $row)
+    {
+    	$stock = lookup($row["symbol"]);
+
+    	if ($stock !== false)
+    	{
+    		$positions[] = [
+    			"name" => $stock["name"],
+    			"symbol" => $row["symbol"],
+    			"shares" => $row["shares"],
+    			"price" => $stock["price"]
+    		];
+    	}
+    }
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    render("portfolio.php", ["title" => "Portfolio", "positions" => $positions,
+    						"cash" => $cash, "username"=> $username]);
 
 ?>
--- psets/7/pset7/views/portfolio.php
+++ psets/7/pset7/views/portfolio.php
@@ -1,3 +1,44 @@
-<div>
-    <iframe allowfullscreen frameborder="0" height="315" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&iv_load_policy=3&rel=0" width="420"></iframe>
+<div id="user_section">
+    <?php
+    	print("<h1>Welcome back <strong>{$username}</strong>!</h1>");
+    	print("<h3>Current cash: <strong>{$cash}</strong></h1>");
+    ?>
+</div>
+
+<br>
+
+<div id="data_table">
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th class="text-center">Company Name</th>
+				<th class="text-center">Symbol</th>
+				<th class="text-center">Shares</th>
+				<th class="text-center">Price</th>
+
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+				if (count($positions) > 0)
+				{
+					foreach($positions as $position)
+		    		{
+		    			print("<tr>");
+		    			print("<td>{$position["name"]}</td>");
+		    			print("<td>{$position["symbol"]}</td>");
+		    			print("<td>{$position["shares"]}</td>");
+		    			print("<td>{$position["price"]}</td>");
+		    			print("</tr>");
+		    		}
+				}
+				
+				else
+				{
+					print("<h5>You don't own any shares at the moment. Go buy some!</h5>");
+				}
+			?>
+		</tbody>
+    </table>
 </div>
