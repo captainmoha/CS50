8acebcf40911caa130821c8cfd42b0dbc4b58bfc
captainmoha <admiralmoha@gmail.com>
2016-11-21 04:47:15 +0200

    comment buy.php

--- psets/7/pset7/public/buy.php
+++ psets/7/pset7/public/buy.php
@@ -11,14 +11,19 @@
 	{
 		$symbol = $_POST["symbol"];
 
+		// make sure the symbol that the user has entered is valid
 		$isStock = lookup($symbol);
 
 		if ($isStock)
 		{
 			$shares = $_POST["shares"];
+
+			// make sure that the user entered a postive number
 			if (preg_match("/^\d+$/", $shares))
-			{
+			{	
+				// get user's currnt cash
 				$cash = CS50::query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"])[0]["cash"];
+				// calculate price of the shares
 				$price = $isStock["price"] * $shares;
 
 				if ($price <= $cash)
@@ -26,6 +31,8 @@
 					// insert new entery into portfolios. if it's there update the number of shares.
 					CS50::query("INSERT INTO portfolios (user_id, symbol, shares) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)", $_SESSION["id"], $isStock["symbol"],
 																	$shares);
+
+					// subtract from users cash to complete the transaction
 					CS50::query("UPDATE users SET cash = cash - ? WHERE id = ?", $price, $_SESSION["id"]);
 					redirect("/");
 				}
